90eac7ae1c6a7b8daa5a111f7d188aeb2b878ac2
Sydney Sharp <sydney.marie.sharp@gmail.com>
2024-05-21 19:16:48 -0500

    Practice: Critters and Croquettes: Day 3

--- walking/llama.py
+++ walking/llama.py
@@ -4,14 +4,18 @@ from datetime import date
 class Llama:
     """Constructs a Llama"""
 
-    def __init__(self, shift):
+    def __init__(self, name, species, shift, food):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
         self.shift = shift
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 
 # when this is called^ provide a name species and a date
